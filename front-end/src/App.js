58f925014cdea6c19322686c38aacc0f9588e348
serban.cobzac <serban.cobzac@feel-it-services.com>
2019-12-08 23:48:46 +0200

    Connected front-end with api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('/train', {
+      method: 'POST'
+    })
+      .then(res => res.json())
+      .then(data => this.setState({data}))
+  }
+  
+  render() {
+    const { data } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {data && data.message}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
