018afacde3d25270fb2a2fe2b89508f49501df3d
medvichi-stefan <medvichi.stefan@gmail.com>
2020-01-03 00:00:24 +0200

    Restructure data proccessing, adding new features

--- api/config.py
+++ api/config.py
@@ -1,3 +1,4 @@
 train_data_path = './../data/train_electricity.csv'
 test_data_path = './../data/test_electricity.csv'
+processed_train_data = './../data/processed_training_data.csv'
 trained_models_dir_path = 'trained_models'
--- api/data_processing.py
+++ api/data_processing.py
@@ -1,26 +1,41 @@
 import pandas as pd
 import config
 from sklearn import preprocessing
-from utils import X_columns
+from utils import X_original_columns as x_columns
 
-def process_data(file_path):
-    returnData = []
-    if file_path is not None:
-        inputData = pd.read_csv(file_path)
-        inputData.drop(axis=1, columns="Date", inplace=True)
-        min_max_scaler = preprocessing.MinMaxScaler()
-        inputData[X_columns] = min_max_scaler.fit_transform(inputData[X_columns])
-        return inputData
-    return returnData
+x_columns = list(x_columns)
+x_columns.remove('Date')
 
-def get_test_data():
-    returnData = []
-    if config.test_data_path is not None:
-        returnData = process_data(config.test_data_path)
-    return returnData
+def process_data(dataframe):
+    new_features_input = add_new_features(dataframe)
+    new_features_input.drop(axis=1, columns="Date", inplace=True)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    new_features_input[x_columns] = min_max_scaler.fit_transform(new_features_input[x_columns])
+    return new_features_input
 
 def get_train_data():
-    returnData = []
-    if config.train_data_path is not None:
-        returnData = process_data(config.train_data_path)
-    return returnData
+    return pd.read_csv(config.processed_train_data)
+
+def add_new_features(dataframe):
+    new_features = ["Year", "Month", "Week", "Day", "Dayofyear", "Dayofweek", "Hour"]
+    one_hot_features = ["Month", "Day", "Week", "Dayofweek", "Hour"]
+    datetime = pd.to_datetime(dataframe.Date, unit="s")
+    for feature in new_features:
+        new_feature = getattr(datetime.dt, feature.lower())
+        if feature in one_hot_features:
+            dataframe = pd.concat([dataframe, pd.get_dummies(new_feature, prefix=feature)], axis=1)
+        else:
+            dataframe[feature] = new_feature
+    return dataframe
+
+def process_training_data():
+    input_data = pd.read_csv(config.train_data_path)
+    new_features_input_data = process_data(input_data)
+
+    return new_features_input_data
+    
+
+if __name__ == '__main__':
+    # process training data
+    df = process_training_data()
+    df.to_csv(config.processed_train_data, index=False)
